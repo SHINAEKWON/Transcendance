daa127522a82c9f4c649c223a157cd58474c8feb
Alex S. Laptop <axse.info@gmail.com>
2025-03-09 13:06:52 +0100

    Started to transform to single page application (spa)

---
+++ login.js
@@ -0,0 +1,32 @@
+function renderLoginPage()
+{
+    const app = document.getElementById("app");
+    
+    app.innerHTML = `
+        <div class="containerNameInput">
+            <h1 class="inputNameHeading">Enter names</h1>
+            <form id="formNames" class="formNames">
+                <input class="inputName inputNameLeft" type="text" id="inputNameLeft" placeholder="Left player" required>
+                <input class="inputName inputNameRight" type="text" id="inputNameRight" placeholder="Right player" required>
+                <button class="btnSubmitName" id="btnSubmitName" type="submit">Start Game</button>
+            </form>
+        </div>
+    `;
+    
+    const formNames = document.getElementById("formNames");
+    formNames.addEventListener("submit", submitNames);
+}
+    
+function submitNames(event)
+{
+    // when clicking on a "Submit" button, prevent it from submitting a form
+    event.preventDefault();
+    
+    const inputNameLeft = document.getElementById("inputNameLeft");
+    const inputNameRight = document.getElementById("inputNameRight");
+
+    const nameLeft = inputNameLeft.value;
+    const nameRight = inputNameRight.value;
+
+    navigateTo("game", { player1: nameLeft, player2: nameRight });
+}
