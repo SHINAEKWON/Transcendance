8ced51d1bbe1616675e1db18d5ede6c90657e0c9
Alex S. 42 <axse.info@gmail.com>
2025-03-15 16:01:53 +0100

    Restructured typescript files

--- backend/game/src/index.ts
+++ backend/game/src/index.ts
@@ -6,6 +6,6 @@ const sayGreeting = (): void => {
     setTimeout(function() {
         let greeting: HTMLElement = document.createElement("p");
         greeting.innerText = "I'm using TypeScript!";
-        document.getElementById('info')?.appendChild(greeting);
+        document.getElementById('app')?.appendChild(greeting);
     }, 3000)
 }
